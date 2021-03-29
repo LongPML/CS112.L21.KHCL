46adb7ee99a6f60da664263e6fa8c32fc3b7ad4a
Long Pml <longpml3421@gmail.com>
2021-03-29 14:26:28 +0700

    Update Add_test

--- Báo cáo chủ đề/Add_test.py
+++ Báo cáo chủ đề/Add_test.py
@@ -6,19 +6,19 @@ from Add import Add
 #Creating class unittest
 class TestAdd(unittest.TestCase):
     def test_1_manual(self):
-        with open("Add.in", 'w+') as f_in:          #Creating new file input
-            with open("Add.out", 'w+') as f_out:    #Creating new file output
-                self.assertEqual(Add(3,5), 8)       #Testing Add function if its work as expected 
-                f_in.write('3 5')                   #Write down test case to input file
-                f_out.write('{}'.format(Add(3,5)))  #Write down result to output file
+        with open("Add1.in", 'w+') as f_in:          # Creating new file input
+            with open("Add1.out", 'w+') as f_out:    # Creating new file output
+                self.assertEqual(Add(3,5), 8)       # Testing Add function if its work as expected 
+                f_in.write('3 5')                   # Write down test case to input file
+                f_out.write('{}'.format(Add(3,5)))  # Write down result to output file
 
     def test_2_auto(self):
-        with open("Add.in", 'a+') as f_in:
-            with open("Add.out", 'a+') as f_out:    
-                for _ in range(100):                   #Random 100 cases
+        for i in range(2, 6):      # Create random 4 new input/output files
+            with open("Add{}.in".format(i), 'w+') as f_in:
+                with open("Add{}.out".format(i), 'w+') as f_out:                  
                     x=random.randint(-100,100)
                     y=random.randint(-100,100)
-                    self.assertEqual(Add(x,y), x+y)    #Comparision with a true function
+                    self.assertEqual(Add(x,y), x+y)    # Comparision with a true function
                     f_in.write('\n{} {}'.format(x,y))
                     f_out.write('\n{}'.format(Add(x,y)))
 
